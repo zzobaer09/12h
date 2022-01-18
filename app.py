5391812aa1312665deffed5cab61af50b2cfefe1
zzobaer09 <zzobaer09@gmail.com>
2022-01-18 11:11:07 +0600

    first

---
+++ app.py
@@ -0,0 +1,6 @@
+for i in range(1,21):
+    if i == 13:
+        pass
+    
+        
+    print(i)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+al):
+    #     if val == 0:
+    #         print("happy new year")
+    #     else:
+    #         pass
+    # newYear(i)
