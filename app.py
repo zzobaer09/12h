dda86b8d9ee45b5714319bafc8aa83a6e72799b9
zzobaer09 <zzobaer09@gmail.com>
2022-01-18 13:36:55 +0600

    all

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-while True:
-    name = input("enter name: ")
-    if name != "":
-        print(name)
-        break
+food = ["pizza" , "hambarger" , "susii" , "ice cream"]
 
+
+food.append("log")
+food.remove("pizza")
+food.pop()
+food.insert(0  , "vanila ice cream")
+food.sort()
+# food.index("ban" , 0)
+food.clear()
+for i in food:
+    print(i)
